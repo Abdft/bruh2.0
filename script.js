7b0568b596632c3ff2ac892bc77f14f9bdff6371
Abdft <110430080+Abdft@users.noreply.github.com>
2024-01-22 16:24:32 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+// Select the menu icon and navigation list
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
-menu.onclick  = () => {
-    menu.classList.toggle('bx-x')
-    navlist.classList.toggle('open')
-}
+// Define a function to toggle classes when the menu icon is clicked
+menu.onclick = () => {
+    // Toggle the 'bx-x' class on the menu icon
+    menu.classList.toggle('bx-x');
+
+    // Toggle the 'open' class on the navigation list
+    navlist.classList.toggle('open');
+};
